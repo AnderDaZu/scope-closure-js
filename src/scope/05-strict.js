75b9f66c6992a1a2a0d0a06c9774a8f729ae89fa
anderDaZu <anderda-96@hotmail.com>
2024-09-03 08:33:03 -0500

    Modo estricto

---
+++ src/scope/05-strict.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// cuando no se declara una variable y se usa el modo estricto esto genera un error ya que se obliga a declarar todas las variables
+pi = 3.1416; //  pi is not defined
+console.log(pi);
